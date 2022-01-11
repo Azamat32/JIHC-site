557d3fb92683e07ddcd90d9d5c84514381c91f77
07almasashimbekov <75465336+07almasashimbekov@users.noreply.github.com>
2022-01-11 16:36:22 +0600

    add navbar socials

--- static/js/script.js
+++ static/js/script.js
@@ -2,42 +2,40 @@ const burgerMenu = document.querySelector('.navbar_burger'); //выбрать г
 const menuList = document.querySelector('.navbar_nav'); //родительский элемент листа меню
 const body = document.querySelector('body'); //это просто body для фиксации
 const links = document.querySelectorAll('navbar_nav-link'); // ссылки меню
-const navbar_language = document.querySelector('.navbar_language');
 
 
 
 burgerMenu.onclick = function showBurger() {
-    this.classList.toggle('activeBurger');
-    menuList.classList.toggle('activeBurger');
-    body.classList.toggle('lockScroll');
-    navbar_language.classList.toggle('activeBurger');
+	this.classList.toggle('activeBurger');
+	menuList.classList.toggle('activeBurger');
+	body.classList.toggle('lockScroll');
 };
 
 const swiper = new Swiper('.swiper_slider-intro', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 30,
-    slidesPerView: 1,
-    clickable: true,
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	spaceBetween: 30,
+	slidesPerView: 1,
+	clickable: true,
 
-    autoplay: {
-        delay: 4000,
-    },
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+	autoplay: {
+		delay: 4000,
+	},
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
 });
